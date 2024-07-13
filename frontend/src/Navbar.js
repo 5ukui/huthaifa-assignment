bdebad009af13b556495ffbef3ef17658476a366
5ukui <m.huthaifa@proton.me>
2024-07-13 09:56:18 +0400

    Modified navbar

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,10 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import logo from './taskerlogo.png';
 import { AuthContext } from './context/AuthContext';
 
 const Navbar = () => {
     const { user, logout } = useContext(AuthContext);
+    
+    // Dropdown menu states
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="bg-white flex fixed w-full top-0 z-50 border-2">
         <div className="p-4 flex justify-between w-full">
@@ -20,16 +27,32 @@ const Navbar = () => {
             <ul className="flex space-x-5 uppercase my-auto">
                 {user ? (
                     <>
-                    <li>
-                        <Link to="/profile" className="hover:text-gray-700">
+                    
+                    <div className="relative">
+                        <button
+                            className="flex items-center justify-center px-4 py-2 text-md font-medium text-black hover:text-gray-900 focus:outline-none focus:text-gray-900 uppercase"
+                            onClick={toggleDropdown}
+                        >
                             {user}
-                        </Link>
-                    </li>
-                    <li>
-                        <button onClick={logout} className="hover:text-gray-700">
-                            LOGOUT
                         </button>
-                    </li>
+                        <div className={`absolute right-0 z-10 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg w-48 ${isOpen ? 'fadeIn' : 'fadeOut'}`}>
+                            <div className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 lowercase">
+                                <li>Signed in as </li>
+                                <li>{user} 👋</li>
+                            </div>
+                            <div className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 text-center">
+                                <Link to="/profile" className="hover:text-gray-700">
+                                    Profile
+                                </Link>
+                            </div>
+                            <div className="px-4 py-2 text-sm text-red-700 hover:bg-gray-100 text-center">
+                                <button onClick={logout} className="">
+                                    LOGOUT
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    
                     </>
                 ) : (
                     <>
